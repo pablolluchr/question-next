4b7aa304ef66d0ad679bb6b19535290342880d03
Pablo <patolluch@gmail.com>
2018-12-11 18:14:14 -0500

    Add timeout
    Timout added to web crawlers so if a URL takes too long to be fetched it discards it. Makes the whole thing run <4s.

--- main.py
+++ main.py
@@ -8,50 +8,77 @@ from pygoogling.googling import GoogleSearch
 from printer import print_scores
 import urllib
 from bs4 import BeautifulSoup
+from functools import wraps
+import errno
+import os
+import signal
 
+class TimeoutError(Exception):
+    pass
+
+def timeout(seconds=10, error_message=os.strerror(errno.ETIME)):
+    def decorator(func):
+        def _handle_timeout(signum, frame):
+            raise TimeoutError(error_message)
+
+        def wrapper(*args, **kwargs):
+            signal.signal(signal.SIGALRM, _handle_timeout)
+            signal.alarm(seconds)
+            try:
+                result = func(*args, **kwargs)
+            finally:
+                signal.alarm(0)
+            return result
+
+        return wraps(func)(wrapper)
+
+    return decorator
 
 #gets html from url and parses it to find answer occurrencies
+@timeout(1)
 def getFrequencies(url_and_answers):
-    url = url_and_answers[0]
-    answer1 = url_and_answers[1]
-    answer2 = url_and_answers[2]
-    answer3 = url_and_answers[3]
-    print(url)
     try:
-        with urllib.request.urlopen(url) as response:
-            html = response.read()
-    except urllib.error.HTTPError:
-        # print("http error")
+        # TODO: add timeout to function so that if a certain page takes too much it ignores it
+        url = url_and_answers[0]
+        answer1 = url_and_answers[1]
+        answer2 = url_and_answers[2]
+        answer3 = url_and_answers[3]
+        try:
+            with urllib.request.urlopen(url) as response:
+                html = response.read()
+        except urllib.error.HTTPError:
+            # print("http error")
+            return [0,0,0]
+        except urllib.error.URLError:
+            # print("url error")
+            return [0,0,0]
+
+
+        soup = BeautifulSoup(html,features="html.parser")
+
+        # kill all script and style elements
+        for script in soup(["script", "style"]):
+            script.extract()    # rip it out
+        # get text
+        
+        #process text (all lower case for example)
+        text = soup.get_text().lower()
+
+        #number of occurrences of each string
+        # frequencies = [text.count(answer1), text.count(answer2),text.count(answer3)]
+        frequencies = [text.count(answer1), text.count(answer2),text.count(answer3)]
+
+        # print(frequencies)
+        print(url)
+        return frequencies
+    except TimeoutError:
         return [0,0,0]
-    except urllib.error.URLError:
-        # print("url error")
-        return [0,0,0]
-
-
-    soup = BeautifulSoup(html,features="html.parser")
-
-    # kill all script and style elements
-    for script in soup(["script", "style"]):
-        script.extract()    # rip it out
-    # get text
-    
-    #process text (all lower case for example)
-    text = soup.get_text().lower()
-
-    #number of occurrences of each string
-    # frequencies = [text.count(answer1), text.count(answer2),text.count(answer3)]
-    frequencies = [text.count(answer1), text.count(answer2),text.count(answer3)]
-
-    # print(frequencies)
-    return frequencies
-
 #peforms ocr on the image at path, searches the answer in google and
     #uses getFrequencies to find the most likely answer
 def find_answer(path):
 
-    number_of_workers = 3
-    number_of_urls = 9
-
+    number_of_workers = 10
+    number_of_urls = 10
 
     #Perform ocr
     start_time = time.time()
